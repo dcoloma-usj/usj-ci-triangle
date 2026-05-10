0cb40b1c45cfc51af080ad95d3165654a33f6081
javimotero7777 <javimotero7777@gmail.com>
2026-05-10 09:54:02 +0200

    style: fix quote style to satisfy eslint

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 var INVALID_ARGUMENTS = 'The arguments were not valid';
 var NOT_A_TRIANGLE = 'Impossible to create a triangle with those sides';
 var EQUILATERAL = 'Equilateral triangle';
-var ISOSCELES = "Isosceles triangle";
+var ISOSCELES = 'Isosceles triangle';
 var SCALENE = 'Scalene triangle';
 
 /*
